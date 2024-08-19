2fe97a6a68c8f901c24e1b64500baef395e4ce58
Walker <166620151+walkerdemonlord@users.noreply.github.com>
2024-08-19 16:27:42 -0300

    update

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const divididoCampo2 = campo2.split('');
 function criptografar (a) {
     for (let i = 0; i < divididoCampo1.length; ++i) {
         divididoCampo1 = divididoCampo1.charCode(i);
-
+        divididoCampo1 = divididoCampo1.map(i => i + 2);
+        divididoCampo1 = divididoCampo1.String.fromCharCode(i);
+        
     }
 }
