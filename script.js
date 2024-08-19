1cc2f838859c225989390b0e4894c0622003a244
Walker <166620151+walkerdemonlord@users.noreply.github.com>
2024-08-19 17:40:45 -0300

    Fazendo Funcionar

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-let campo1 = document.querySelector(".campo1").value;
-let campo2 = document.querySelector(".campo2").value;
-const divididoCampo1 = campo1.split('');
-const divididoCampo2 = campo2.split('');
+// Selecionar os campos de entrada
+let campo1 = document.querySelector(".campo1");
+let campo2 = document.querySelector(".campo2");
 
-function criptografar (a) {
-    for (let i = 0; i < divididoCampo1.length; ++i) {
-        divididoCampo1 = divididoCampo1.charCode(i);
-        divididoCampo1 = divididoCampo1.map(i => i + 2);
-        divididoCampo1 = divididoCampo1.String.fromCharCode(i);
-        
-    }
+// Função para criptografar o texto
+function criptografar() {
+    let texto = campo1.value;
+    let divididoCampo1 = texto.split('');
+    let criptografado = divididoCampo1.map(char => {
+        let codigoChar = char.charCodeAt(0);
+        let codigoCriptografado = codigoChar + 2; // Adicionar 2 ao código do caractere
+        return String.fromCharCode(codigoCriptografado); // Converter de volta para caractere
+    }).join(''); // Juntar os caracteres em uma string
+
+    // Atualizar o campo2 com o texto criptografado
+    campo2.value = criptografado;
+    campo1.value = ""; // Limpar campo1
+}
+
+// Função para descriptografar o texto
+function descriptografar() {
+    let texto = campo2.value;
+    let divididoCampo2 = texto.split('');
+    let descriptografado = divididoCampo2.map(char => {
+        let codigoChar = char.charCodeAt(0);
+        let codigoDescriptografado = codigoChar - 2; // Subtrair 2 do código do caractere
+        return String.fromCharCode(codigoDescriptografado); // Converter de volta para caractere
+    }).join(''); // Juntar os caracteres em uma string
+
+    // Atualizar o campo1 com o texto descriptografado
+    campo1.value = descriptografado;
+    campo2.value = ""; // Limpar campo2
 }
