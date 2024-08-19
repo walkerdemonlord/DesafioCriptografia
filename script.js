48d806b0d3c0a62cffd3b705af2bd845e2670416
Walker <166620151+walkerdemonlord@users.noreply.github.com>
2024-08-19 16:17:48 -0300

    Implementação JS

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let campo1 = document.querySelector(".campo1");
-let campo2 = document.querySelector(".campo2");
+let campo1 = document.querySelector(".campo1").value;
+let campo2 = document.querySelector(".campo2").value;
+const divididoCampo1 = campo1.split('');
+const divididoCampo2 = campo2.split('');
 
+function criptografar (a) {
+    for (let i = 0; i < divididoCampo1.length; ++i) {
+        divididoCampo1 = divididoCampo1.charCode(i);
 
+    }
+}
